55d40eb19be5e5bd88c5f8e8dacd9f346c2e1d6c
JuHyun419 <zzang9ha@naver.com>
2021-04-09 22:27:29 +0900

    docs: Rename directory bakcend -> back

---
+++ back/src/main/java/com/github/oneline/onelinecourse/OnelineCourseApplication.java
@@ -0,0 +1,13 @@
+package com.github.oneline.onelinecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnelineCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnelineCourseApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/github/oneline/onelinecourse/OnelineCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oneline.onelinecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnelineCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
