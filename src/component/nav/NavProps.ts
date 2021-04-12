6f3a459c95de589d1d73999e0fbaaee1f36bf99a
Yoonsang Lee <highp0912@protonmail.ch>
2021-04-12 13:53:58 +0900

    fix: Delete unwanted files from tracked files

--- src/component/nav/NavProps.ts
+++
@@ -1,5 +0,0 @@
-interface NavProps {
- 
-}
-
-export default NavProps;
--- src/component/nav/SignInNav.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./_Nav.scss";
-
-const SignInNav: React.FC<{ children?: JSX.Element }> = ({ children }) => {
-  return <div className="nav-signIn">{children}</div>;
-};
-
-export default SignInNav;
