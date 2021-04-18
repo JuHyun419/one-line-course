6354540200fbefb768bfee84489f522f838fb3e4
bsy3764 <bsy33764@gmail.com>
2021-04-18 14:42:08 +0900

    style: fix Bookmark model - fix bookmark entity

--- back/src/main/java/com/github/oneline/onelinecourse/model/bookmark/Bookmark.java
+++ back/src/main/java/com/github/oneline/onelinecourse/model/bookmark/Bookmark.java
@@ -1,5 +1,7 @@
 package com.github.oneline.onelinecourse.model.bookmark;
 
+import com.github.oneline.onelinecourse.model.lecture.Lecture;
+import com.github.oneline.onelinecourse.model.user.User;
 import lombok.*;
 import javax.persistence.*;
 
@@ -8,17 +10,20 @@ import javax.persistence.*;
 @Entity // JPA에서 사용한 Entity class  선언
 @Table(name = "Bookmarks")  // JPA에서 DB를 매칭할 테이블명
 public class Bookmark {
+
     @Id // 기본키(PK)로 지정
     @NonNull
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 생성되도록 하기위해
     private Long id;
 
-    @Column(length = 50, nullable = false)
-    private Long userId;
-
-    @Column(length = 20, nullable = false)
-    private Long lectureId;
-
     @Column(length = 500, nullable = false)
     private String createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id")
+    private Lecture lecture;
 }
