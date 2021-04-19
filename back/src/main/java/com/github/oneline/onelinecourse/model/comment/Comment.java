9f30e52634c4ca1edac45ab3c3fa8f0ee907a74d
JuHyun419 <zzang9ha@naver.com>
2021-04-19 16:18:55 +0900

    feat: Update comment

--- back/src/main/java/com/github/oneline/onelinecourse/model/comment/Comment.java
+++ back/src/main/java/com/github/oneline/onelinecourse/model/comment/Comment.java
@@ -1,6 +1,8 @@
 package com.github.oneline.onelinecourse.model.comment;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.github.oneline.onelinecourse.model.lecture.Lecture;
+import com.github.oneline.onelinecourse.model.user.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
@@ -23,7 +26,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 1000)
     private String content;
 
     @Column
@@ -36,31 +39,25 @@ public class Comment {
     @JoinColumn(name = "lecture_id")
     private Lecture lecture;
 
-    /*@ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private Users users;*/
+    private User user;
 
     @Builder
-    public Comment(String content, Lecture lecture/*, Users users */) {
-        this(null, content, null, null, lecture/*, users*/);
+    public Comment(String content, Lecture lecture, User user) {
+        this(null, content, null, null, lecture, user);
     }
 
     public Comment(Long id, String content, LocalDateTime createdAt,
-                   LocalDateTime updatedAt, Lecture lecture/*, Users users*/) {
+                   LocalDateTime updatedAt, Lecture lecture, User user) {
         checkNotNull(content, "content must be provided");
-        checkNotNull(lecture, "lectures must be provided");
-        /*checkNotNull(users, "users must be provided");*/
 
         this.id = id;
         this.content = content;
         this.createdAt = defaultIfNull(createdAt, LocalDateTime.now());
         this.updatedAt = defaultIfNull(updatedAt, LocalDateTime.now());
         this.lecture = lecture;
-        /*this.users = users;*/
-    }
-
-    public void addLecture(Lecture lecture) {
-        this.lecture = lecture;
+        this.user = user;
     }
 
     public void updateContent(String content) {
@@ -68,12 +65,26 @@ public class Comment {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
                 .append("id", id)
                 .append("content", content)
-                .append("lectures", lecture)
-                /*.append("users", users)*/
+                .append("lecture", lecture)
+                .append("user", user)
                 .toString();
     }
 }
