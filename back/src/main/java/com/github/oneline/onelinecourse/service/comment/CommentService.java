f102c525028240cf65384cba8ee510a36ee412a2
JuHyun419 <zzang9ha@naver.com>
2021-04-19 17:44:10 +0900

    feat: Update comment service

--- back/src/main/java/com/github/oneline/onelinecourse/service/comment/CommentService.java
+++ back/src/main/java/com/github/oneline/onelinecourse/service/comment/CommentService.java
@@ -36,9 +36,9 @@ public class CommentService {
 
     private Comment convertToComment(final Comment comment, final String userId, final Long lectureId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("userId: " + userId + "의 유저가 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("userId: " + userId + "의 유저가 존재하지 않습니다"));
         Lecture lecture = lectureRepository.findById(lectureId)
-                .orElseThrow(() -> new IllegalArgumentException("lectureId: " + lectureId + "의 강의가 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("lectureId: " + lectureId + "의 강의가 존재하지 않습니다"));
 
         return Comment.builder()
                 .content(comment.getContent())
@@ -52,7 +52,7 @@ public class CommentService {
         checkArgument(commentId > 0, "commentId must be positive number");
 
         final Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new IllegalArgumentException("commentId: " + commentId + "번에 해당하는 댓글이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("commentId: " + commentId + "번에 해당하는 댓글이 존재하지 않습니다"));
 
         comment.updateContent(requestComment.getContent());
     }
@@ -62,7 +62,7 @@ public class CommentService {
         checkArgument(commentId > 0, "commentId must be positive number");
 
         final Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new IllegalArgumentException(commentId + "번에 해당하는 댓글이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException(commentId + "번에 해당하는 댓글이 존재하지 않습니다"));
 
         commentRepository.delete(comment);
     }
