bc8cb410d6cb9836207c8358fa2a262dd7606e2e
JuHyun419 <zzang9ha@naver.com>
2021-04-25 15:05:06 +0900

    feat: Update lecture select  - if no content(nothing data): HttpStatus code: 204(no content)  - else exist contents: HttpStatus code: 200(ok)

--- back/src/main/java/com/github/oneline/onelinecourse/controller/lecture/LectureController.java
+++ back/src/main/java/com/github/oneline/onelinecourse/controller/lecture/LectureController.java
@@ -3,6 +3,7 @@ package com.github.oneline.onelinecourse.controller.lecture;
 import com.github.oneline.onelinecourse.service.lecture.LectureService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +20,15 @@ public class LectureController {
     @GetMapping("/lectures")
     @ApiOperation(value = "강의 전체 조회")
     public ResponseEntity<List<LectureResponse>> getLectures() {
-        return ResponseEntity.ok(
-                lectureService.findAllLectures()
-                        .stream()
-                        .map(LectureResponse::new)
-                        .collect(Collectors.toList())
-        );
+        List<LectureResponse> response = lectureService.findAllLectures()
+                .stream()
+                .map(LectureResponse::new)
+                .collect(Collectors.toList());
+
+        HttpStatus statusCode = response.isEmpty()
+                ? HttpStatus.NO_CONTENT
+                : HttpStatus.OK;
+
+        return new ResponseEntity<>(response, statusCode);
     }
 }
