ca1be87fe4a2465c8f42a3c3748afe50d4064b71
JuHyun419 <zzang9ha@naver.com>
2021-04-26 21:32:03 +0900

    refactor: Remove not used import

--- back/src/main/java/com/github/oneline/onelinecourse/controller/lecture/LectureController.java
+++ back/src/main/java/com/github/oneline/onelinecourse/controller/lecture/LectureController.java
@@ -3,7 +3,6 @@ package com.github.oneline.onelinecourse.controller.lecture;
 import com.github.oneline.onelinecourse.service.lecture.LectureService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
