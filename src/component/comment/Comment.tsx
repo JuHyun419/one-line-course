c3be46d6b45918614fe9a6509c35a3c807f99c7c
yoonsang lee <highp0912@protonmail.ch>
2021-04-29 13:22:02 +0900

    feat: Add basic comments structure

---
+++ src/component/comment/Comment.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Comment = () => {
+  // TODO: retrieve user data
+  // TODO: retrieve bookmark data
+  // TODO: dispatch to bookmark store on interacting comment
+  return (
+    <div>
+      {/* user thumbnail */}
+      {/* user name */}
+      {/* comment - contents */}
+      {/* comment - delete button */}
+      {/* comment - fix button */}
+      {/* comment - createdAt */}
+    </div>
+  );
+};
+
+export default Comment;
