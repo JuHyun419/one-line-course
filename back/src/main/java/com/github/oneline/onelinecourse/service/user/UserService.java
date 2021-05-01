62ed46f34c977276706fe1f8af6d3a1d766a7664
JuHyun419 <zzang9ha@naver.com>
2021-05-01 15:42:44 +0900

    refactor: Delete not used import

--- back/src/main/java/com/github/oneline/onelinecourse/service/user/UserService.java
+++ back/src/main/java/com/github/oneline/onelinecourse/service/user/UserService.java
@@ -4,8 +4,6 @@ import com.github.oneline.onelinecourse.model.user.User;
 import com.github.oneline.onelinecourse.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 // 트랜잭션, 도메인 기능 간의 순서를 보장하는 Service
 @Service
 public class UserService {
@@ -19,7 +17,6 @@ public class UserService {
     // 처음 로그인인지 확인
     public User searchUser(String userId) {
         User user = userRepository.findById(userId)
-//                .orElseThrow(() -> new IllegalArgumentException("userId: " + userId + "의 유저가 존재하지 않습니다."));
                 .orElse(null);
         return user;
     }
