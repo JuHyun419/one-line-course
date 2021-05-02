68c5ec80ebfe473737afd59d4443e7bbbe60b242
JuHyun419 <zzang9ha@naver.com>
2021-05-02 15:18:47 +0900

    feat: Add swagger config(resource handlers)

--- back/src/main/java/com/github/oneline/onelinecourse/configuration/Swagger2Config.java
+++ back/src/main/java/com/github/oneline/onelinecourse/configuration/Swagger2Config.java
@@ -2,6 +2,8 @@ package com.github.oneline.onelinecourse.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class Swagger2Config {
+public class Swagger2Config implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
@@ -30,6 +32,15 @@ public class Swagger2Config {
                 .description("Hello world")
                 .version("v1.0")
                 .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html**")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
