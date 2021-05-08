6607c171827eb404f24be1748f8239acba0867f5
JuHyun419 <zzang9ha@naver.com>
2021-05-08 14:20:45 +0900

    feat: Add ErrorResponse class

---
+++ back/src/main/java/com/github/oneline/onelinecourse/common/error/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.github.oneline.onelinecourse.common.error;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+
+    private String message;
+
+    private ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public static ErrorResponse of(String message) {
+        return new ErrorResponse(message);
+    }
+
+}
