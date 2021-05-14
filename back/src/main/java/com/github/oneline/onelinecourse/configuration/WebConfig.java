4bc5012c6233b75e2b37e9689ea1eb50a22fe90c
JuHyun419 <zzang9ha@naver.com>
2021-05-14 18:46:28 +0900

    refactor: Update cors config

--- back/src/main/java/com/github/oneline/onelinecourse/configuration/WebConfig.java
+++ back/src/main/java/com/github/oneline/onelinecourse/configuration/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:1234");
+                .allowedOrigins("http://localhost:1234")
+                .allowedMethods("*"); // default: GET, POST, HEAD
     }
 
     @Override
