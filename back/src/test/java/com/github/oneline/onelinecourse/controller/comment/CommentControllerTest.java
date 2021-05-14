3e7fb5b5774eb85b0098bfbd37a4c238c105a356
JuHyun419 <zzang9ha@naver.com>
2021-05-14 20:07:37 +0900

    refactor: Update test code

--- back/src/test/java/com/github/oneline/onelinecourse/controller/comment/CommentControllerTest.java
+++ back/src/test/java/com/github/oneline/onelinecourse/controller/comment/CommentControllerTest.java
@@ -1,64 +1,48 @@
 package com.github.oneline.onelinecourse.controller.comment;
 
-import com.github.oneline.onelinecourse.repository.comment.CommentRepository;
+import com.github.oneline.onelinecourse.common.error.exception.LectureNotFoundException;
+import com.github.oneline.onelinecourse.common.error.exception.UserNotFoundException;
+import com.github.oneline.onelinecourse.service.comment.CommentService;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CommentControllerTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    private CommentService commentService;
 
-    @Autowired
-    private CommentRepository commentRepository;
-
-    @DisplayName("조회 데이터가 없는 강의 댓글을 조회한다.")
+    @DisplayName("존재하지 않는 강의로 댓글을 조회하면 예외를 반환한다.")
     @Test
-    void getNoDataLectureComment() {
+    void non_Exist_Lecture_Throw_Exception() {
         // given
-        final int notExistsLectureId = 99999;
-        final String url = "http://localhost:" + port + "/comments/lectures/" + notExistsLectureId;
+        final Long notExistsLectureId = 99999L;
 
-        // when
-        ResponseEntity<List> responseEntity = testRestTemplate.getForEntity(url, List.class);
-
-        // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-        assertThat(responseEntity.getBody()).isNull();
+        // when & then
+        assertThatThrownBy(() -> commentService.findAllLectureComments(notExistsLectureId))
+                .isInstanceOf(LectureNotFoundException.class)
+                .hasMessageContaining("존재하지 않는 강의");
     }
 
-    @DisplayName("조회 데이터가 없는 유저 댓글을 조회한다.")
+    @DisplayName("존재하지 않는 유저로 댓글을 조회하면 예외를 반환한다.")
     @Test
-    void getNoDataUserComment() {
+    void non_Exist_User_Throw_Exception() {
         // given
         final String noExistsUserId = RandomStringUtils.randomAlphanumeric(100);
-        final String url = "http://localhost:" + port + "/comments/users/" + noExistsUserId;
 
-        // when
-        ResponseEntity<List> responseEntity = testRestTemplate.getForEntity(url, List.class);
+        // when & then
+        assertThatThrownBy(() -> commentService.findAllUserComments(noExistsUserId))
+                .isInstanceOf(UserNotFoundException.class)
+                .hasMessageContaining("존재하지 않는 사용자");
 
-        // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-        assertThat(responseEntity.getBody()).isNull();
     }
 
 }
