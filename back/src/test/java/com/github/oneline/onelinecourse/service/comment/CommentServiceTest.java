4772563785645c84fc89974b8616bc2d775bda3f
JuHyun419 <zzang9ha@naver.com>
2021-06-18 10:52:14 +0900

    feat: Add profiles maven

--- back/src/test/java/com/github/oneline/onelinecourse/service/comment/CommentServiceTest.java
+++ back/src/test/java/com/github/oneline/onelinecourse/service/comment/CommentServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.ConstraintViolationException;
@@ -20,6 +21,7 @@ import javax.validation.ConstraintViolationException;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@ActiveProfiles("prod")
 @Transactional
 @SpringBootTest
 class CommentServiceTest {
